936afcf3b46850907b9a3acd8bfdf9eefa0d1ec0
Brian Richie Sr <brianrichiesr@gmail.com>
2023-10-26 09:34:09 -0700

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, match_word):
+        self.match_word = sorted(list(match_word.lower()))
+    
+    def match(self, word_list):
+        result = []
+        for word in word_list:
+            sorted_word = sorted(list(word.lower()))
+            if sorted_word == self.match_word:
+                result.append(word)
+        return result
